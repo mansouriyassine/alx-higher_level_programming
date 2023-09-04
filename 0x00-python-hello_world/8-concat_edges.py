e7079610f6615413f905cc4d454913704450cfc8
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-04 13:01:21 -0700

    concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[21:41])
+        language that combines remarkable power with very clear syntax"
+str = str[21:41]
+print(str)
