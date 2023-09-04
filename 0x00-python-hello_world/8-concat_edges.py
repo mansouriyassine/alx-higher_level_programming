86229e75eb7d614098a3c9d6a1eca8c6926637e7
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-04 12:49:06 -0700

    8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[69:98] + str[98:100] + str[113:120] + str[0:6])
+        language that combines remarkable power with very clear syntax"
+
+substring1 = str[39:67]
+substring2 = str[107:112]
+substring3 = str[:6]
+
+result = substring1 + substring2 + substring3
+print(result)
