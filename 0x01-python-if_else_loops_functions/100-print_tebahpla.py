36883232469220aa1396dca2d45da560e1460636
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-05 10:58:57 -0700

    14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,6 +4,6 @@ for char_code in range(ord('z'), ord('A') - 1, -1):
     char = chr(char_code)
     if char_code % 2 == 1:
         char = char.upper()
-    print(char, end='')
+    print("{}".format(char), end='')
 
 print()
