b37c159df15b0df4dc22a05496e58dff24dd3aa4
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-05 08:41:26 -0700

    2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) not in ('e', 'q'):
-        print("{:c}".format(letter), end='')
+alphabet = ''
 
-print()
+for letter in range(ord('a'), ord('z') + 1):
+    # Append the letter to the alphabet string if it's not 'e' or 'q'
+    if chr(letter) not in ('e', 'q'):
+        alphabet += chr(letter)
+
+print("{}".format(alphabet), end='')
