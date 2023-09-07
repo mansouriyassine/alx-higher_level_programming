7cfbb9c6df715372984ebfbd89291a6856eaab66
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-07 07:59:25 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the count and list of command-line arguments."""
+    """Print the number of and list of arguments"""
     import sys
 
-    num_arguments = len(sys.argv) - 1
+    argv = sys.argv
+    argc = len(argv) - 1
 
-    if num_arguments == 0:
-        print("No arguments provided.")
-    elif num_arguments == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(num_arguments))
+    print("{}{}".format(
+        argc,
+        " argument" + ("s:" if argc != 1 else ":") if argc > 0 else ".")
+    )
 
-    for index, argument in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(index, argument))
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
