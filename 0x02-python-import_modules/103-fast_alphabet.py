417c14e109dfcea7bf944e131e4340e0054dde9a
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-07 08:47:01 -0700

    9

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 import string
-
-
-def print_uppercase_alphabet():
-    print(string.ascii_uppercase)
-
-
-if __name__ == "__main__":
-    print_uppercase_alphabet()
+print(getattr(string, 'ascii_uppercase'))
