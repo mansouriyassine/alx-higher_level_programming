6e4ded6cec578d6f03a7bdc7148684b559e459cd
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-09 05:22:35 -0700

    5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    result = ""
+    
+
+    for char in my_string:
+        # Check if the character is not 'c' or 'C' and add it to the result
+        if char != 'c' and char != 'C':
+            result += char
+    
+    return result
+
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
