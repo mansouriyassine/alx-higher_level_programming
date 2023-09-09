6838d5f0a918f63ed450bd4c95d82c6024b72bfa
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-09 06:59:20 -0700

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,12 +5,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b1, b2 = tuple_b[:2] if len(tuple_b) >= 2 else (0, 0)
     result = (a1 + b1, a2 + b2)
     return result
-
-if __name__ == "__main__":
-    tuple_a = (1, 89)
-    tuple_b = (88, 11)
-    new_tuple = add_tuple(tuple_a, tuple_b)
-    print(new_tuple)
-
-    print(add_tuple(tuple_a, (1,)))
-    print(add_tuple(tuple_a, ()))
