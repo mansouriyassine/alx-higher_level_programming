423b71f9cab556e2798768385a0f06262376c026
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-09 05:28:25 -0700

    5 - 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
     result = ""
-    
 
     for char in my_string:
         # Check if the character is not 'c' or 'C' and add it to the result
         if char != 'c' and char != 'C':
             result += char
-    
-    return result
 
+
+return result
 
 if __name__ == "__main__":
     print(no_c("Best School"))
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d} ".format(num), end="")
+        print()  # Print a newline after each row
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
