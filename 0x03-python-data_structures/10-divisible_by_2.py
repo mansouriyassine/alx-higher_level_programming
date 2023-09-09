1968d5edc9f7142ddbbe8c776eeb1f48b4314ca0
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-09 07:20:40 -0700

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    result_list = []
+    
+    for num in my_list:
+        is_divisible = num % 2 == 0
+        result_list.append(is_divisible)
+    
+    return result_list
