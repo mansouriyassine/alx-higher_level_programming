c8e8ef5d58da2ec42b5df60c10e9e01647d1f53b
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-09 08:27:17 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,14 +5,14 @@ if __name__ == '__main__':
     sys.argv.pop(0)
     argvlength = len(sys.argv)
 
-    if (argvlength == 0):
+    if argvlength == 0:
         print("0 arguments.")
-    elif (argvlength == 1):
+    elif argvlength == 1:
         print("1 argument:")
         print("{:d}: {}".format(len(sys.argv), sys.argv[0]))
     else:
         print("{:d} arguments:".format(argvlength))
         number = 1
         for argument in sys.argv:
-                print("{:d}: {}".format(number, argument))
-                number += 1
+            print("{:d}: {}".format(number, argument))
+            number += 1
