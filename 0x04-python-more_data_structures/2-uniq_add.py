790a4391bebf502af1252e9ccb1920f159df6f33
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-13 14:11:21 -0700

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    unique_integers = set()
-    sum_of_unique = 0
-    for num in my_list:
-        if num not in unique_integers:
-            unique_integers.add(num)
-            sum_of_unique += num
-    return sum_of_unique
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 1, 4, 2, 5]
-    result = uniq_add(my_list)
-    print("Result: {:d}".format(result))
-
+    u, s = set(), 0
+    [u.add(n) for n in my_list if n not in u or u.add(n)]
+    return sum(u)
