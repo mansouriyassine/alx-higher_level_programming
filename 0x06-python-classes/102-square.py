e20e7c5f82d2a3814a5dc3e77eeb51800ceeee5f
mansouriyassine <mansouri.yassine1@gmail.com>
2023-09-27 14:49:28 -0700

    103

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int or float): The size of the square.
+        """
         self.size = size
 
     @property
     def size(self):
-        return self._size
+        """Get/set the size of the square."""
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -14,25 +24,50 @@ class Square:
             raise TypeError("size must be a number")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self._size = value
+        self.__size = value
 
     def area(self):
-        return self._size ** 2
+        """Calculate the area of the square."""
+        return self.__size ** 2
 
     def __eq__(self, other):
+        """Compare two squares for equality (==)."""
         return self.area() == other.area()
 
     def __ne__(self, other):
+        """Compare two squares for inequality (!=)."""
         return self.area() != other.area()
 
     def __lt__(self, other):
+        """Compare two squares for less than (<)."""
         return self.area() < other.area()
 
     def __le__(self, other):
+        """Compare two squares for less than or equal to (<=)."""
         return self.area() <= other.area()
 
     def __gt__(self, other):
+        """Compare two squares for greater than (>)."""
         return self.area() > other.area()
 
     def __ge__(self, other):
+        """Compare two squares for greater than or equal to (>=)."""
         return self.area() >= other.area()
+
+
+if __name__ == "__main__":
+    s_5 = Square(5)
+    s_6 = Square(6)
+
+    if s_5 < s_6:
+        print("Square 5 < Square 6")
+    if s_5 <= s_6:
+        print("Square 5 <= Square 6")
+    if s_5 == s_6:
+        print("Square 5 == Square 6")
+    if s_5 != s_6:
+        print("Square 5 != Square 6")
+    if s_5 > s_6:
+        print("Square 5 > Square 6")
+    if s_5 >= s_6:
+        print("Square 5 >= Square 6")
