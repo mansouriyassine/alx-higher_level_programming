d87eb965e259716cb08c2617c7a58c98a291f251
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-02 16:45:47 -0700

    0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """
+    This is a simple class that represents a rectangle.
+
+    Attributes:
+        No attributes are defined in this class.
+    """
+    pass
