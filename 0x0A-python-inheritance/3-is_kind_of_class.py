f7dce814f8aadd307200b8b095d6377fb797efa4
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-09 11:37:17 -0700

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""check if object is an instance of, or inherits from, a specified class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if object is an instance of or inherits fro a specified class."""
+    return isinstance(obj, a_class)
