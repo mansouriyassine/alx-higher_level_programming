187796281ac66f5c195ab536a6ac80618ee52a37
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 15:33:25 -0700

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-read_file = __import__('0-read_file').read_file
+"""Read file """
 
-read_file("my_file_0.txt")
+
+def read_file(filename=""):
+    """Reads content.
+
+    Args:
+        filename (str): name of file
+
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
