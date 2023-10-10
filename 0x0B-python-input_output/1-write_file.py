c03cf00fe9322af53754d706e70b6d0bcf241039
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 02:26:28 -0700

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as file_to_write:
+        nb_characters = file_to_write.write(text)
+    return nb_characters
+
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
