3f0ea6dd1f896d6440deaf3c7e6226d6e03991d4
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 15:14:52 -0700

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
+read_file = __import__('0-read_file').read_file
 
-def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
+read_file("my_file_0.txt")
