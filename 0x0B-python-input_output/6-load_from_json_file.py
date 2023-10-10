cefe218f1ad9721986072bff19687f5da431bdf4
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 16:33:19 -0700

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function to load an object from a JSON file."""
+import json
+
+
+def load_from_json_file(filename):
+    """Load an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file to load.
+
+    Returns:
+        The Python object loaded from the JSON file.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
