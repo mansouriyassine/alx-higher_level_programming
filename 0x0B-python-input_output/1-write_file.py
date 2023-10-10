6442eae0b5b92c3621b00ad2fa7fa9b3b8f0f2c7
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 16:07:10 -0700

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """File Writer: Write a string to a UTF-8 text file."""
 
+
 def write_file(filename="", text=""):
     """
     Write the provided text to a UTF-8 text file.
@@ -16,6 +17,10 @@ def write_file(filename="", text=""):
         chars_written = file.write(text)
         return chars_written
 
+
 if __name__ == "__main__":
-    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    filename = "my_first_file.txt"
+    text = "This School is so cool!\n"
+    nb_characters = write_file(filename, text)
+
     print(nb_characters + len("\n"))
