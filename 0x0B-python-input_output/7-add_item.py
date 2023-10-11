c1f2ce2b656a7219333d77fcf0f5aedbd4d1e315
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 17:19:25 -0700

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 """Add items to a JSON file."""
 
-from sys import argv
+import sys
 from json_file_utils import save_to_json_file, load_from_json_file
 
-arguments = argv[1:]
 
-try:
-    existing_content = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    existing_content = []
+def add_items_to_json(file_path, items):
+    try:
+        existing_content = load_from_json_file(file_path)
+    except FileNotFoundError:
+        existing_content = []
 
-for argument in arguments:
-    existing_content.append(argument)
+    existing_content.extend(items)
 
-save_to_json_file(existing_content, "add_item.json")
+    save_to_json_file(existing_content, file_path)
+
+    print("Items added successfully!")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./7-add_item.py <item1> <item2> ...")
+    else:
+        items_to_add = sys.argv[1:]
+        add_items_to_json("add_item.json", items_to_add)
