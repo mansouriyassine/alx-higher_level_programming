24b4245cbe05939808f5f463c7806fc8d012aab7
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 17:32:06 -0700

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+"""
+This script defines a function to generate Pascal's Triangle.
+"""
 
 
-def generate_pascals_triangle(n):
-    """Generate Pascal's Triangle of size n as a list of lists.
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle of size n.
 
-    Args:
-        n (int): The number of rows for the Pascal's Triangle.
-
-    Returns:
-        list: A list of lists representing Pascal's Triangle.
+    Returns a list of lists representing the triangle.
     """
     if n <= 0:
         return []
 
     triangle = [[1]]
-    while len(triangle) != n:
-        current_row = triangle[-1]
-        new_row = [1]
-
-        for i in range(len(current_row) - 1):
-            new_row.append(current_row[i] + current_row[i + 1])
-
-        new_row.append(1)
-        triangle.append(new_row)
+    while len(triangle) < n:
+        row = [1]
+        last_row = triangle[-1]
+        for i in range(len(last_row) - 1):
+            row.append(last_row[i] + last_row[i + 1])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
