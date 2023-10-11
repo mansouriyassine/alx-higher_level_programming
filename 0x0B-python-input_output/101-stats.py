b3c3e915a48b01f60084b511d12c288bcb983193
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-10 17:24:26 -0700

    101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-
+"""
+This script reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
+it prints the following statistics:
+- Total file size up to that point.
+- Count of read status codes up to that point.
 """
 
-def print_metrics(file_size, status_codes):
+
+def print_stats(size, status_codes):
     """Print accumulated metrics.
 
     Args:
-        file_size (int): The accumulated file size.
+        size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
     """
-    print("Total file size: {}".format(file_size))
-    for code in sorted(status_codes):
-        print("{}: {}".format(code, status_codes[code]))
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
 
-    file_size = 0
+    size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    line_count = 0
+    count = 0
 
     try:
         for line in sys.stdin:
-            if line_count == 10:
-                print_metrics(file_size, status_codes)
-                line_count = 1
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
             else:
-                line_count += 1
+                count += 1
 
             line = line.split()
 
             try:
-                file_size += int(line[-1])
+                size += int(line[-1])
             except (IndexError, ValueError):
                 pass
 
@@ -50,8 +52,8 @@ if __name__ == "__main__":
             except IndexError:
                 pass
 
-        print_metrics(file_size, status_codes)
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
-        print_metrics(file_size, status_codes)
+        print_stats(size, status_codes)
         raise
