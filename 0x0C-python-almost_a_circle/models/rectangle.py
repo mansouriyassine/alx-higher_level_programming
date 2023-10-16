fc97b5ecb2d199f113e2b5e750c6951e810ac122
mansouriyassine <mansouri.yassine1@gmail.com>
2023-10-16 13:17:06 -0700

    2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -108,7 +108,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns a string representation of the object."""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " \
+               f"{self.__width}/{self.__height}"
 
     def update(self, *args, **kwargs):
         """Updates the attributes of the rectangle."""
