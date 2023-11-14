23773e68aed575be915596274335ea7d2bab1175
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-14 11:51:25 -0800

    102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
--- Script to display the top three cities based on average temperature during July and August
-SELECT city, AVG(temperature) AS avg_temp
+-- Script to display the top three cities based
+-- on average temperature during July and August
+SELECT city, AVG(value) AS avg_temp
 FROM hbtn_0c_0.temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY avg_temp DESC
 ORDER BY avg_temp DESC LIMIT 3;
