03e1d429d183e1f9b723502bcbf324bef65afab6
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-14 11:48:22 -0800

    103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,5 @@
--- Script to display the maximum temperature for each state, ordered by state name
+-- Script to display the maximum temperature for each state
+-- ordered by state name
 SELECT state, MAX(temperature) AS max_temp
 FROM hbtn_0c_0.temperatures
 GROUP BY state
