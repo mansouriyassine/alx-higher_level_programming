b9943f3cb8f967355ecf3e2c49c261185fd3f936
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-14 11:44:03 -0800

    102

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,6 @@
--- Script to display average temperature by city in descending order
-SELECT city, AVG(temperature) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+-- This script calculates the mean temperature for each city from the 'temperatures' table
+-- Data is sourced from the 'hbtn_0c_0' database
+SELECT city, AVG(value) AS average_temperature
+FROM hbtn_0c_0.temperatures
+GROUP BY city
+ORDER BY average_temperature DESC;
