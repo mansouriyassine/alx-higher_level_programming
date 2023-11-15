599b267fde28377a5aef880a208481197d6213c1
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-15 09:15:55 -0800

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
