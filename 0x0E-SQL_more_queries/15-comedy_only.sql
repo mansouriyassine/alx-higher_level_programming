5aef8f3ae672fd88ef951820f97e08c87a837704
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-15 09:44:37 -0800

    15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
--- List all Comedy shows
-SELECT tv_shows.title
+-- List all Comedy shows sql
+SELECT tv_shows.title AS title
 FROM tv_shows
-JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-JOIN genres
-ON tv_show_genres.genre_id = genres.id
-WHERE genres.name = 'Comedy'
-ORDER BY tv_shows.title;
+     JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
