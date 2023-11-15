71ef3ce6f3798a93ceedd0495438b491bb997324
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-15 09:37:00 -0800

    12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
--- List shows without a genre
-SELECT tv_shows.title
+-- List all shows with genre IDs, include NULL for no genre
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title;
+WHERE genre_id is NULL
+ORDER BY title, genre_id;
