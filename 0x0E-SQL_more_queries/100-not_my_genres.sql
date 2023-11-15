3325486bda24b6a2c889ecd9cd3e86d2e16a87e5
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-15 09:54:16 -0800

    100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,2 +1,16 @@
--- List genres not linked to Dexter
-SELECT name FROM genres WHERE id NOT IN (SELECT genre_id FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE title = 'Dexter') ORDER BY name;
+-- List genres not linked to serie Dexter
+SELECT tv_genres.name AS name
+FROM tv_genres
+LEFT JOIN
+(
+	SELECT tv_genres.id, name
+  FROM tv_genres
+	JOIN tv_show_genres
+	     ON tv_genres.id = tv_show_genres.genre_id
+	JOIN tv_shows
+	     ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter"
+	ORDER BY tv_genres.id
+) dexter_genres ON dexter_genres.id = tv_genres.id
+WHERE dexter_genres.id is NULL
+ORDER BY name;
