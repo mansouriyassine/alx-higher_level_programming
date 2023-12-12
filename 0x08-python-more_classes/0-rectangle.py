200c828ea4c1346db7be60ed78c1228cf70a015d
mansouriyassine <mansouri.yassine1@gmail.com>
2023-12-12 02:04:43 +0000

    Commit message describing your changes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,23 @@
-#!/usr/bin/python3
+ass Rectangle:
+    """Defines a Rectangle class."""
+    
+    def __init__(self, width, height):
+        """Initialize the Rectangle with the given width and height."""
+        self.width = width
+        self.height = height
 
-class Rectangle:
-    pass
+    def area(self):
+        """Calculate and return the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculate and return the perimeter of the rectangle."""
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Return a human-readable string representation of the rectangle."""
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def __repr__(self):
+        """Return a string representation of the rectangle that can be used to recreate it."""
+        return f"Rectangle({self.width}, {self.height})"
