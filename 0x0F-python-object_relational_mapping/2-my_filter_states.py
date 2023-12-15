2cec322b5e4d53945c34743f8288fbd406de57a7
mansouriyassine <mansouri.yassine1@gmail.com>
2023-12-15 12:16:10 -0800

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument, safe from MySQL injections.
+This script displays all values in the states table of hbtn_0e_0_usa where
+name matches the argument
 """
 
 import MySQLdb
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
     cur = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state_search,))
+    query = ("SELECT * FROM states WHERE name = '{}' "
+             "ORDER BY id ASC").format(state_search)
+    cur.execute(query)
 
     query_rows = cur.fetchall()
     for row in query_rows:
