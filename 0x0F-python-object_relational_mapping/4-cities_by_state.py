373567ee7aed64f7808e50d0ac99c00ca8e69dc7
mansouriyassine <mansouri.yassine1@gmail.com>
2023-12-15 12:25:24 -0800

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
     cur = db.cursor()
-    
+
     query = """
-    SELECT cities.id, cities.name, states.name 
-    FROM cities 
-    JOIN states ON cities.state_id = states.id 
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC
     """
     cur.execute(query)
