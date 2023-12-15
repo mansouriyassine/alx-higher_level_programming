ccad330eb9b9cebeeff6c84fdbec7df34df958d0
mansouriyassine <mansouri.yassine1@gmail.com>
2023-12-15 12:19:36 -0800

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,40 +1,32 @@
 #!/usr/bin/python3
+"""
+This script lists all cities from the database hbtn_0e_4_usa
+along with their corresponding state names.
+"""
+
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-def list_cities(username, password, db_name):
-    """
-    Lists all cities from the database
-    """
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
     cur = db.cursor()
-
+    
     query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
+    SELECT cities.id, cities.name, states.name 
+    FROM cities 
+    JOIN states ON cities.state_id = states.id 
     ORDER BY cities.id ASC
     """
     cur.execute(query)
 
-    for row in cur.fetchall():
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
 
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        list_cities(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-        )
