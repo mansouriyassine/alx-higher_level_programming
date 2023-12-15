4ba03bc0bbf52155d8b4e29116e32ab7ca2a5dc8
mansouriyassine <mansouri.yassine1@gmail.com>
2023-12-15 06:53:58 -0800

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script lists all states with names starting with 'N' from the database 
+This script lists all states with names starting with 'N' from the database
 hbtn_0e_0_usa
 """
 
