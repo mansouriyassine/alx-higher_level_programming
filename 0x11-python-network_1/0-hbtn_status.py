444cb6b2da92f6946576e463e4ae024f598b1478
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 07:14:29 -0800

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.parse
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type:', type(html))
