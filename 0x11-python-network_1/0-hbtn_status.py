59a4e66d59d164f4881758fffa663712acf56500
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 07:12:15 -0800

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 # Fetches a URL using urllib and prints the body response
 import urllib.request
+import urllib.parse
 
 
 if __name__ == "__main__":
-    # URL to be fetched
-    url = "https://alx-intranet.hbtn.io/status"
-
-    with urllib.request.urlopen(url) as response:
-        response_content = response.read()
-
-        print("Body response:")
-        print("\t- type: {}".format(type(response_content)))
-        print("\t- content: {}".format(response_content))
-        print("\t- utf8 content: {}".format(response_content.decode("utf-8")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('UTF-8'))
