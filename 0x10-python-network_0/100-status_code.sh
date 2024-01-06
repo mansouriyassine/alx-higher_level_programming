e78fa4c640c5fb7546f4630666b0e54870669a70
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 06:32:40 -0800

    100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a request to the URL passed as an argument and displays only the status code of the response.
-curl -o /dev/null -sw '%{http_code}' "$1"
+# Sends a request to the URL passed as an argument displays only the status code of the response.
+curl -s -o /dev/null -w '%{http_code}' "$1"
