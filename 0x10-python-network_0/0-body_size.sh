ecef2f4f94b53d203ba0ed9a4989a19bed109e01
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 05:46:23 -0800

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Script to fetch the content length from a URL's response headers
+# This script takes a URL as an argument, sends a request to that URL, 
+# and displays the size of the body of the response in bytes.
 
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
+curl -s "$1" -o response_body -w '%{size_download}\n'
