6e340881d1fc88b5c81e5314770243434be2d2f4
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 13:04:25 -0800

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script sends a request to a URL and displays
-the value of the 'X-Request-Id' variable in the response header.
+Sends a request to a URL and displays
+the value of the 'X-Request-Id' variable
+in the response header.
 """
 import requests
 import sys
@@ -11,11 +12,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
 
-    if response.status_code == 200:
-        if 'X-Request-Id' in response.headers:
-            x_request_id = response.headers['X-Request-Id']
-            print(x_request_id)
-        else:
-            print("X-Request-Id header not found in the response")
-    else:
-        print("Request failed with status code:", response.status_code)
+    print(response.headers.get('X-Request-Id'))
