5d5ba9b1681376ebb69cda4445343e9497ed976f
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 06:48:33 -0800

    101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL passed as the first argument,
-# with the contents of a file passed as the second argument, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -X POST -d @"$2" "$1"
+# This script sends a json post  reuqest to a URL passed as the first argument
+curl -sX POST "$1" -H "Content-Type: application/json" -d @"$2"
