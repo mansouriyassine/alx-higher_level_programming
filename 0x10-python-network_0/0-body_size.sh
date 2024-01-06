30b09faa3c1345a37f93a2c34d2fa5751261767d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 06:06:46 -0800

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL, 
-# and displays the size of the body of the response in byte
-
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+# This script takes a URL and displays the size of the body of the response
+curl -s "$1" -o /dev/null -w '%{size_download}\n'
