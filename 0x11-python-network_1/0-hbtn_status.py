4f5b3a6e2f829c0108ebc4d5082966996a2fd2a8
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 07:16:31 -0800

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-# Fetches a URL using urllib and prints the body response
+"""
+A script that fetches https://alx-intranet.hbtn.io/status
+"""
 import urllib.request
-import urllib.parse
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type:', type(html))
-        print('\t- content:', html)
-        print('\t- utf8 content:', html.decode('UTF-8'))
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        # Read the content of the response
+        content = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
