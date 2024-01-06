74c1d8ac36451585411504dcfca895d141639497
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 05:55:04 -0800

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a GET request to a URL passed as an argument and displays the body of the response.
 
-curl -sL "$1"
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
