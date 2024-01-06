c8e5e498a6bde4969ba6f3417533dadb34c6ec83
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 13:07:15 -0800

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script sends a POST request to a URL with
-an email parameter and displays the response body.
+Sends a POST request to a URL with an email
+parameter and displays the response body.
 """
 import requests
 import sys
@@ -12,9 +12,7 @@ if __name__ == "__main__":
     email = sys.argv[2]
 
     payload = {'email': email}
+
     response = requests.post(url, data=payload)
 
-    if response.status_code == 200:
-        print("Your email is:", response.text)
-    else:
-        print("Request failed with status code:", response.status_code)
+    print(response.text)
