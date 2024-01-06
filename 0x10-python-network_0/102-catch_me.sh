cd96c734bc895c32b18c394b6278da854cae123e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 06:51:11 -0800

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script makes a request to a given endpoint that results in a specific server response.
-curl -sX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" -d "user_id=98"
+# sends a JSON POST request to a URL
+curl "0.0.0.0:5000/catch_me" -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98"
