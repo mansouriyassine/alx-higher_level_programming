b0a30996e46fc61354214ffc5e89c59fffe482d1
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-06 06:17:37 -0800

    6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        cond1 = mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]
+        cond2 = (mid == len(list_of_integers) - 1 or
+                 list_of_integers[mid + 1] <= list_of_integers[mid])
+        if cond1 and cond2:
+            return list_of_integers[mid]
+        if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
