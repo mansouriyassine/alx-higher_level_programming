c92b15cfa018a500905c00cf35b56637aba4d496
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-30 05:41:08 -0800

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,34 +2,34 @@
 
 const request = require('request');
 
-function fetchCharacter(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(JSON.parse(body).name);
-            }
-        });
+function fetchCharacter (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
     });
+  });
 }
 
 const movieID = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
 request(url, async (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
-        return;
-    }
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
 
-    const film = JSON.parse(body);
-    for (const characterUrl of film.characters) {
-        try {
-            const name = await fetchCharacter(characterUrl);
-            console.log(name);
-        } catch (error) {
-            console.error('error:', error);
-        }
+  const film = JSON.parse(body);
+  for (const characterUrl of film.characters) {
+    try {
+      const name = await fetchCharacter(characterUrl);
+      console.log(name);
+    } catch (error) {
+      console.error('error:', error);
     }
+  }
 });
