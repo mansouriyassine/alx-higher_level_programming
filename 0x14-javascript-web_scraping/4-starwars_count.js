0e2a53c3662f1e4fc3d6253cf9cb94094f55ffb2
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-30 05:45:07 -0800

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,20 +4,20 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
-        return;
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+  const wedgeAntillesID = '18';
+  let count = 0;
+
+  films.forEach(film => {
+    if (film.characters.some(url => url.includes(`/people/${wedgeAntillesID}/`))) {
+      count++;
     }
+  });
 
-    const films = JSON.parse(body).results;
-    const wedgeAntillesID = '18';
-    let count = 0;
-
-    films.forEach(film => {
-        if (film.characters.some(url => url.includes(`/people/${wedgeAntillesID}/`))) {
-            count++;
-        }
-    });
-
-    console.log(count);
+  console.log(count);
 });
