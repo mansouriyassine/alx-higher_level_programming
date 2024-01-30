8346fa4264714414ade44ad8c329c8ab5a26d09d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-30 05:11:34 -0800

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+  const wedgeAntillesID = 'https://swapi-api.alx-tools.com/api/people/18/';
+  let count = 0;
+
+  films.forEach(film => {
+    if (film.characters.includes(wedgeAntillesID)) {
+      count++;
+    }
+  });
+
+  console.log(count);
+});
