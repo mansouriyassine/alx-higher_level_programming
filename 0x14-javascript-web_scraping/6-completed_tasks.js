b96506f61806703b71abb3570b053fd43b34bbe0
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-30 05:18:27 -0800

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach(task => {
+    if (task.completed) {
+      if (completedTasks[task.userId] === undefined) {
+        completedTasks[task.userId] = 0;
+      }
+      completedTasks[task.userId]++;
+    }
+  });
+
+  console.log(completedTasks);
+});
