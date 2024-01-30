e1aecc767330617eb45489a72b904ab1b612a488
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-30 05:33:21 -0800

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,37 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+function fetchCharacter(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+async function main() {
+  const movieID = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+  request(url, async (error, response, body) => {
+    if (error) {
+      console.error('error:', error);
+      return;
+    }
+
+    const film = JSON.parse(body);
+    const characterPromises = film.characters.map(fetchCharacter);
+
+    try {
+      const characters = await Promise.all(characterPromises);
+      characters.forEach(character => console.log(character));
+    } catch (error) {
+      console.error('error:', error);
+    }
+  });
+}
